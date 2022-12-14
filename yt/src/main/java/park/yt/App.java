cf9237bceda29b21d46de026217833b800618049
soldesk <soldesk@DESKTOP-VPD9AJE>
2022-12-14 18:10:04 +0900

    배열생성해서 넣으면...?되나...? 넣기만하면됨이제..

--- yt/src/main/java/park/yt/App.java
+++ yt/src/main/java/park/yt/App.java
@@ -14,9 +14,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class App {
 	public static void main(String[] args) {
 		List<Object> data = new ArrayList<>();
-		String[] imsi = new String[5];
+		List<Object> nameData = new ArrayList<>();
+		List<Object> camData = new ArrayList<>();
+		String[] name1;
+		Object[] cam1;
 		String online = null;
-		Object[] stream;
 		try {
 
 			Path path = Paths.get(System.getProperty("user.dir"), "sec/main/resouce/chromdriver.exe");
@@ -39,47 +41,35 @@ public class App {
 
 			List<WebElement> personName = person.findElements(By.xpath("//*[@class='XEazBc adnwBd']"));// 이름
 			List<WebElement> camOn = person.findElements(By.xpath("//*[@class='Gv1mTb-aTv5jf']"));// 비디오
-			stream=personName.stream().distinct().toArray();
-			int i=0;
+			//stream = personName.stream().distinct().toArray();
+			int i = 0;
 			for (i = 0; i < personName.size(); i++) { // 이름 찾기
 				online = personName.get(i).getText();
-				
 
-				if (!online.equals("")) {
-					data.add(online);// 이름을 데이터 리스트에 저장
-					System.out.println(online);
-				}
+				// if (!online.equals("")) {
+				data.add(online);// 이름을 데이터 리스트에 저장
+				System.out.println(online);
+				// }
 			}
-			System.out.println("총 인원 :"+i+"명");
-			
-			int j=0;
+			System.out.println("총 인원 :" + i + "명");
+
+			int j = 0;
 			System.out.println("----------------캠 킨 사람 ---------------------------");
 			for (i = 0; i < camOn.size(); i++) {// 비디오 있는인원 찾기
 				String cam = camOn.get(i).getAriaRole();
 				String mot = camOn.get(i).findElement(By.xpath("./../../../..")).getText();// 비디오의 부모 이름이 나옴.
 				if (cam.equals("Video")) {
-					
 					++j;
-					data.add(cam);// 비디오 정보를 데이타에 저장
-					System.out.println(mot+" ="+cam);
+					nameData.add(mot);
+					camData.add(cam);
+					System.out.println(mot + " =" + cam);
 				}
 			}
-				System.out.println(j+"명");
-			
-			j=0;
-			System.out.println("--------------------캠안킨사람----------------------");
-			for (i = 0; i < camOn.size(); i++) {// 비디오 있는인원 찾기
-				String cam = camOn.get(i).getAriaRole();
-				String mot = camOn.get(i).findElement(By.xpath("./../../../..")).getText();// 비디오의 부모 이름이 나옴.
-				if (cam.equals("none")) {
-					++j;
-					data.add(cam);// 비디오 정보를 데이타에 저장
-					System.out.println(mot+" ="+cam);
-				}
-			}
-			System.out.println(j+"명");
+			System.out.println(j + "명");
 
-			System.out.println(data);
+			System.out.println(data);//전체 리스트
+			System.out.println(nameData);//캠 인원 이름
+			//System.out.println(camData);// 캠킨 여부
 		} catch (Exception e) {
 			e.printStackTrace();
 		}

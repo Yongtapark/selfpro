de9eba2979dfc5ae67e8065e33591263cb305beb
soldesk <soldesk@DESKTOP-VPD9AJE>
2023-01-02 09:40:43 +0900

    접속할때 try catch문 넣어야함

--- yt/src/main/java/park/yt/googlecheck_Simple.java
+++ yt/src/main/java/park/yt/googlecheck_Simple.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class googlecheck {
+public class googlecheck_Simple {
 	public static void main(String[] args) throws IOException {
 		List<Object> data = new ArrayList<>();
 		List<Object> nameData = new ArrayList<>();
